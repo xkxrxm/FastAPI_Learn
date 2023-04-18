0f75937dbce9bfabd887d3b7337a1443cc77303d
xkx <431968368@qq.com>
2023-04-18 20:54:23 +0800

    学习了如何在fastapi中链接MySQL数据库

--- 第六章/示例6_6/project/sql_app/database.py
+++ 第六章/示例6_6/project/sql_app/database.py
@@ -5,7 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # 第二步，创建数据连接引擎
-engine = create_engine("mysql://root:www.isoftstone.CoM@localhost/cat")
+# 数据库连接串的格式为“数据库类型：//用户名：密码@主机/数据库实例”
+engine = create_engine("mysql://root:123456@localhost/cat")
 # 第三步，创建本地会话
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # 第四步，创建数据模型基础类
